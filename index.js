35f96714119542c16f37bd832a410559ed50ac06
Erlan <okay11007@gmail.com>
2017-08-27 12:54:31 +0600

    Removed let

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 'use strict';
 
-let s_settings = require('./settings');
+var s_settings = require('./settings');
 
-let settings = _interopRequireWildcard(s_settings);
+var settings = _interopRequireWildcard(s_settings);
 
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { let newObj = {}; if (obj != null) { for (let key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
-let io = require('socket.io')(settings.PORT);
+var io = require('socket.io')(settings.PORT);
 
-let chat = io.of('/chat').on('connection', function (socket) {
+var chat = io.of('/chat').on('connection', function (socket) {
   console.log('connected');
 
   socket.emit('create a room');
 
   socket.on('a message', function (data) {
     console.log(data.room + ': ' + data.message);
-    let room = data.room;
-    let message = data.message;
+    var room = data.room;
+    var message = data.message;
     socket.to(room).broadcast.emit('a message', message);
   }).on('join a room', function (data) {
     console.log(data.room);
